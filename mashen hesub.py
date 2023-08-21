53b855b9e15eceadc6bdf344e855d155de4cbdaf
Narges-Safaeefard <139851899+Narges-Safaeefard@users.noreply.github.com>
2023-08-21 07:11:12 -0700

    Add files via upload

---
+++ mashen hesub.py
@@ -0,0 +1,24 @@
+import math
+import random
+
+while True:
+
+ num1=int(input("adad aval ra vared kon "))
+
+ num2=int (input("adad dovom ra vared koni"))
+  
+
+ p=input("yekee az in  amalgar ha ra vared kon (cos,sin)")
+
+ if p =="sin":
+
+    num3= math.sin(num1)
+    print(num3)
+
+ elif p =="cos":
+    num3= math.cos(num2)
+    print(num3)
+
+
+
+    
